cab2b11798ecddb4259e9e9ad1d3eb5c01178028
Noel Fernandez <noelsamadegrao@gmail.com>
2025-10-23 22:00:52 +0200

    Ejercicio buscador (AD) Y Crud y tareaEntregable (DI)

--- Acceso a Datos/Tema2_Buscador/src/Logica/Main.java
+++ Acceso a Datos/Tema2_Buscador/src/Logica/Main.java
@@ -1,39 +1,127 @@
 package Logica;
 
-import java.io.File;
+import java.io.*;
+import java.util.Scanner;
 
 /**
  * Pide una cadena de texto y, a partir de una ruta, revisa todo lo que encuentra:
- * -Las carpetas buesca en ella otras carpetas o .java
- * -Los .java los lee enteros buscando cadenas de texto
+ * - Las carpetas busca en ella otras carpetas o .java
+ * - Los .java los lee enteros buscando cadenas de texto
  * Cada vez que encuentra la cadena en un .java se muestra
- * por pantalla la ruta completa del archivo y el numero de lnea donde se encuentra y la fila en si
+ * por pantalla la ruta completa del archivo y el número de línea donde se encuentra y la fila en sí
  */
 public class Main {
 
-	public static final File raiz = new File("C:\\Users\\noels\\OneDrive\\Desktop\\2ºDAM");
-	
-	public static void main(String[] args) {
-		
-		analizaCarpeta(raiz);
-	}
+    public static final File raiz = new File("C:\\Users\\noels\\OneDrive\\Desktop\\2ºDAM\\2-DAM");
+    public static Contador contador = new Contador();
 
-	/**
-	 * Revisa una carpeta y:
-	 * -Las carpetas: se vuelven a llamar a si mismas (recusivo)
-	 * -Los .java: llama a analizaJava
-	 */
-	private static void analizaCarpeta(File raiz2) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	/**
-	 * -Los .java los lee enteros buscando cadenas de texto
-	 * Cada vez que encuentra la cadena en un .java se muestra
-	 * por pantalla la ruta completa del archivo y el numero de lnea donde se encuentra y la fila en si
-	 */
-	private static void analizaJava(File fichero, String cadena) {
+    public static void main(String[] args) {
+        String cadena = null;
+        if (args.length > 0) {
+            cadena = args[0];
+        } else {
+            System.out.println("Introduce la palabra o frase a buscar:");
+            Scanner sc = new Scanner(System.in);
+            cadena = sc.nextLine();
+            sc.close();
+        }
+        System.out.println("Buscando la cadena: " + cadena);
+        analizaCarpeta(raiz, cadena);
+        System.out.println(contador);
+    }
 
-	}
+    /**
+     * Revisa una carpeta y:
+     * - Las carpetas: se vuelven a llamar a sí mismas (recursivo)
+     * - Los .java: llama a analizaJava
+     */
+    private static void analizaCarpeta(File raiz, String cadena) {
+        contador.carpetaVisitada();
+
+        for (String nombreHijo : raiz.list()) {
+            File hijo = new File(raiz, nombreHijo);
+            if (hijo.isDirectory()) {
+                analizaCarpeta(hijo, cadena);
+            } else if (hijo.isFile() && nombreHijo.endsWith(".java")) {
+                analizaJava(hijo, cadena);
+            }
+        }
+    }
+
+    /**
+     * Lee los .java buscando la cadena
+     * Muestra ruta completa, número de línea y el contenido
+     */
+    private static void analizaJava(File java, String cadena) {
+        contador.archivoVisitado();
+
+        boolean huboCoincidenciaEnArchivo = false;
+        int numLinea = 0;
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader(java));
+            String fila;
+            while ((fila = br.readLine()) != null) {
+                numLinea++;
+                Main.contador.lineaLeida();
+
+                if (fila.toLowerCase().contains(cadena.toLowerCase())) {
+                    if (!huboCoincidenciaEnArchivo) {
+                        Main.contador.archivoConCoincidencia();
+                        huboCoincidenciaEnArchivo = true;
+                    }
+                    System.out.printf("-> %s, línea %d: %s%n",
+                            java.getAbsolutePath(), numLinea, fila);
+                    Main.contador.coincidencia();
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("No se encontró el archivo: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Error al leer: "+ e.getMessage());
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static class Contador {
+        private int carpetasVisitadas;
+        private int archivosVisitados;
+        private int lineasLeidas;
+        private int coincidencias;
+        private int archivosCoincidencias;
+
+        public void carpetaVisitada() {
+        	carpetasVisitadas++;
+        }
+        public void archivoVisitado() {
+        	archivosVisitados++;
+        }
+        public void lineaLeida() {
+        	lineasLeidas++;
+        }
+        public void coincidencia() {
+        	coincidencias++;
+        }
+        public void archivoConCoincidencia() {
+        	archivosCoincidencias++;
+        }
+
+        public String toString() {
+            return String.format(
+                    "Carpetas visitadas: %d%nArchivos visitados: %d%nLíneas leídas: %d%n" +
+                    "Coincidencias encontradas: %d%nArchivos con coincidencias: %d",
+                    carpetasVisitadas, archivosVisitados, lineasLeidas, coincidencias, archivosCoincidencias
+            );
+        }
+    }
 }
+
--- Desarrollo Interfaces/Tema2/jdbcDam/src/TiendaInformatica/AppTiendaInformatica.java
+++ Desarrollo Interfaces/Tema2/jdbcDam/src/TiendaInformatica/AppTiendaInformatica.java
@@ -1,8 +1,10 @@
 package TiendaInformatica;
 
+import java.io.FileWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class AppTiendaInformatica {
@@ -13,27 +15,120 @@ public class AppTiendaInformatica {
 	private static int buscarPorFabricante(int fabricante) {
 		String sql="Select * from articulo where fabricante=?";
 		ResultSet rs = null;
-		
+		int contador=0;
 		try {
 			ps = cn.prepareStatement(sql);
 			ps.setInt(1, fabricante);
 			rs = ps.executeQuery();
+			
 			while (rs.next()) {
-				int codigo = rs.getInt("codigo");
-				String nombre = rs.getString("nombre");
-				double precio = rs.getDouble("precio");
-				int fab = rs.getInt("fabricante");
-				Articulo art = new Articulo(codigo, nombre, precio, fab);
+				//gestionamos el resltSet
+				Articulo art = new Articulo(rs.getInt("codigo"), rs.getString("nombre"), rs.getDouble("precio"), rs.getInt("fabricante"));
+				System.out.println("**********************");
 				System.out.println(art.toString());
+				System.out.println("**********************");
+				contador++;
 			}
+			ps.close();
+			rs.close();
+			
 		} catch (Exception e) {
 			System.out.println("Error: " + e.getMessage());
-		} 
-		return 0;
+		}
+		return contador;
+		
 	}
+	
+	private static boolean insertarArticulo(Articulo art) {
+		String sql="Insert into articulo values (?,?,?,?)";
+		int resultado=0;
+		try {
+			ps= cn.prepareStatement(sql);
+			ps.setInt(1, art.getCodigo());
+			ps.setString(2, art.getNombre());
+			ps.setDouble(3, art.getPrecio());
+			ps.setInt(4, art.getFabricante());
+			resultado= ps.executeUpdate();
+			
+			ps.close();
+			
+			if (resultado == 0) {
+				return false;
+			} else {
+				return true;
+			}
+			
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+		return false;
+		
+		
+	}
+	
+	private static int borrarArticuloPorPrecio(String sql) {
+		Statement st = null;
+		int nRegistros=0;
+		try {
+			st = cn.createStatement();
+			nRegistros = st.executeUpdate(sql);
+            st.close();			
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		return nRegistros;
+	}
+	
+	public static int incrementarPrecioFabricante(double incremento, int fabricante) {
+		String sql = "Update articulo set precio=precio+precio*? where fabricante=?";
+		int nRegistros = 0;
+		try {
+			ps = cn.prepareStatement(sql);
+			ps.setDouble(1, incremento);
+			ps.setInt(2, fabricante);
+			nRegistros = ps.executeUpdate();
+			ps.close();
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		return nRegistros;
+		
+	}
+	
+	public static boolean copiaSeguridadTexto() {
+		String sql = "Select * from articulo";
+		Statement st = null;
+		FileWriter fw = null;
+		ResultSet rs = null;
+		Articulo art;
+		Boolean realizado = false;
+		
+		try {
+			fw = new FileWriter("CopiaSeguridadArticulos.txt");
+			fw.write("Codigo;Nombre;Precio;Fabricante\n");
+			st = cn.createStatement();
+			rs = st.executeQuery(sql);
+			while (rs.next()) {
+				realizado = true;
+				art = new Articulo(rs.getInt("codigo"), rs.getString("nombre"), rs.getDouble("precio"), rs.getInt("fabricante"));
+				fw.write(art.toString()+"\n");
+			}
+			fw.write("Fin de la copia de Seguridad");
+			rs.close();
+			st.close();
+			fw.close();
+			
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		return false;
+	}
+	
 	public static void main(String[] args) {
 		int opcion=0;
 		Scanner sc = new Scanner(System.in);
+		Articulo art=null;
 		try {
 			AccesoDatos ac = new AccesoDatos("TiendaDam");
 			cn = ac.getConnection();
@@ -53,12 +148,33 @@ public class AppTiendaInformatica {
 					System.out.println("Se han mostrado "+ buscarPorFabricante(fabricante) + " articulos." );
 					break;
 				case 2:
+					//pediremos los datos
+					art = new Articulo(2,"monitor", 150.0, 3);
+					if(insertarArticulo(art)) {
+						System.out.println("Articulo insertado correctamente");
+					} else {
+						System.out.println("Error al insertar el articulo");
+					}
 					break;
 				case 3:
+					System.out.println("Introduzca el precio maximo para borrar articulos:");
+					double precio = sc.nextDouble();
+					String sql="Delete from articulo where precio>" + precio;
+					System.out.println("Se han borrado " + borrarArticuloPorPrecio(sql)+ " registros.");
 					break;
 				case 4:
+					System.out.println("Introduzca el incremento de precio");
+					double incremento = sc.nextDouble();
+					System.out.println("Introduzca el codigo de fabricante");
+					int fab = sc.nextInt();
+					System.out.println("Se han actualizado " + incrementarPrecioFabricante(incremento, fab) + " registros.");
 					break;
 				case 5:
+					if (copiaSeguridadTexto()) {
+						System.out.println("Copia de seguridad realizada correctamente");
+					} else {
+						System.out.println("Error al realizar la copia de seguridad");
+					}
 					break;
 				case 0:
 					System.out.println("Saliendo...");
